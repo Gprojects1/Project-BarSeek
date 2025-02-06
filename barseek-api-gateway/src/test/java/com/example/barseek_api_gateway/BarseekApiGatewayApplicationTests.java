88d091522b26dc1f3549aa37e75958c0253ee533
Daniil Kornishev <doddadid@gmail.com>
2025-02-06 16:11:13 +0300

    added initial api-gateway

---
+++ barseek-api-gateway/src/test/java/com/example/barseek_api_gateway/BarseekApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.barseek_api_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarseekApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
