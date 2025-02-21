831c9cccfdabb75131432a67ed92166698cce6c5
Daniil Kornishev <doddadid@gmail.com>
2025-02-21 18:03:36 +0300

    dev auth service...

--- barseek-auth-mservice/src/main/java/com/example/barseek_auth_mservice/service/UserService.java
+++ barseek-auth-mservice/src/main/java/com/example/barseek_auth_mservice/service/UserService.java
@@ -2,8 +2,11 @@ package com.example.barseek_auth_mservice.service;
 
 
 import com.example.barseek_auth_mservice.exception.customExceptions.UserNotFoundException;
+import com.example.barseek_auth_mservice.kafka.events.UserCreatedEvent;
+import com.example.barseek_auth_mservice.kafka.producer.KafkaProducerService;
 import com.example.barseek_auth_mservice.model.entity.User;
 import com.example.barseek_auth_mservice.repository.UserRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -18,11 +21,21 @@ public class UserService implements UserDetailsService {
 
     private final UserRepository userRepository;
 
+    private final KafkaProducerService kafkaProducerService;
+
     public Optional<User> findByEmail(String email) {
         return userRepository.findByEmail(email);
     }
 
+    @Transactional
     public User addNew(User user) {
+        UserCreatedEvent userCreatedEvent = UserCreatedEvent.builder()
+                .id(user.getId())
+                .role(user.getRole())
+                .email(user.getEmail())
+                .build();
+        kafkaProducerService.sendUserCreatedEvent(userCreatedEvent);
+
         return userRepository.save(user);
     }
 
